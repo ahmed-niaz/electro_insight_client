d81a2d3e331d5ee1f425ad4290cb8074f426172b
ahmed-niaz <niazahmed.an@gmail.com>
2024-05-13 15:05:20 +0600

    almost complete the recent Quires section

--- src/components/RecentQueries.jsx
+++ src/components/RecentQueries.jsx
@@ -1,11 +1,60 @@
+import PropTypes from 'prop-types'
+const RecentQueries = ({ query }) => {
+  const { imgURL, product_name, brand, queries_title, reason, user_info } =
+    query;
+  //   console.log(user_info);
+  return (
+    <main>
+      <div className="flex flex-col items-center justify-center w-full mx-auto ">
+        <div
+          className="w-full h-[400px] bg-gray-300 bg-center bg-cover rounded-lg shadow-md"
+          style={{ backgroundImage: `url(${imgURL})` }}
+        ></div>
 
+        <div className="w-11/12 border-2 border-red-500 -mt-10 overflow-hidden  rounded-lg shadow-lg dark:bg-white">
+          <div className="flex justify-center items-center gap-2">
+            <h3 className="py-2 font-bold tracking-wide text-center text-black uppercase dark:text-black">
+              {product_name}
+            </h3>
+            <span className="px-3 py-1 text-xs text-white bg-black rounded-full ">
+              {brand}
+            </span>
+          </div>
 
-const RecentQueries = () => {
-    return (
-        <div>
-            
+          <div className="flex justify-around items-center">
+            <div>
+              <p className="text-xl font-extrabold">
+                Reason:{" "}
+                <span className="font-bold text-gray-500">{reason}</span>
+              </p>
+              <p className="text-xl font-extrabold">
+                {" "}
+                Query Title:{" "}
+                <span className="font-bold text-gray-500">{queries_title}</span>
+              </p>
+              <p className="text-xl font-extrabold">
+                Date Posted:{" "}
+                <span className="font-bold text-gray-500">
+                  {new Date(user_info?.current_time).toDateString()}
+                </span>
+              </p>
+            </div>
+            {/* user data */}
+            <div>
+              <p className="text-gray-400 font-bold">{user_info?.name}</p>
+              <div className="avatar">
+                <div className="w-24 rounded-xl">
+                  <img src={user_info?.profile} />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </main>
+  );
 };
-
+RecentQueries.propTypes = {
+    query: PropTypes.object
+};
 export default RecentQueries;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
 import Slider from "../components/Slider";
-
+import axios from "axios";
+import RecentQueries from "../components/RecentQueries";
 const Home = () => {
+  const { user } = useState();
+  const [recentQuires, setRecentQuires] = useState([]);
+  useEffect(() => {
+    getData();
+  }, [user]);
+
+  const getData = async () => {
+    const { data } = await axios(`${import.meta.env.VITE_API_URL}/queries`);
+    setRecentQuires(data);
+  };
   return (
     <main>
-      <Slider/>
-      <h2>Electro Insight</h2>
+      <Slider />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 container mx-auto my-24">
+        {recentQuires.slice(0, 7).map((query) => (
+          <RecentQueries key={query._id} query={query} />
+        ))}
+      </div>
     </main>
   );
 };
